5d243888e8e3fdb8f46ec7f02e6547fa8abe5931
Pruthvi Kalidindi <Pruthvi.Kalidindi@infor.com>
2020-04-11 23:00:52 +0530

    Web API - using Token Based Authentication.

---
+++ WebapiTokenAuth/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WebapiTokenAuth.Controllers
+{
+    public class UserController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/user/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok($"Hi, time now: {DateTime.Now.ToString()}");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/user/authenticate")]
+        public IHttpActionResult GetForAuthenticate()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok($"Hello {identity.Name}!, time now: {DateTime.Now.ToString()}");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/user/authorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok($"Hello {identity.Name}!, time now: {DateTime.Now.ToString()}");
+        }
+    }
+}
---
+++ WebapiTokenAuth/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebapiTokenAuth.Startup))]
+
+namespace WebapiTokenAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var tokenAuthServerProvider = new TokenAuthServerProvider();
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(5),
+                Provider = tokenAuthServerProvider
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
